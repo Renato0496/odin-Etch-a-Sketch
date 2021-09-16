f1f444120547c755df84733fe5a7d8806bbdb354
Renato Alvarado <riar96@gmail.com>
2021-09-16 17:17:24 -0500

    Solved the script problem and positioning with grid

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector('.game');
 
+function getGrid() {
+    for (i = 0; i < 256; i++) {
 
-const div = document.createElement('div');
-container.appendChild(div);
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        container.appendChild(div);
+    }
+};
+getGrid();
