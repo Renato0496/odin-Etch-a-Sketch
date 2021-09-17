5814d4f33df3fad1bf88fa294c260770c1ce4b2a
Renato Alvarado <riar96@gmail.com>
2021-09-17 13:04:28 -0500

    Adding the hover fucntion and the reset button function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('.game');
+const number = document.querySelector('.number');
+const sqr = document.getElementsByClassName('square');
+const resetBut = document.querySelector('.reset')
 
 function getGrid() {
     for (i = 0; i < 256; i++) {
@@ -8,4 +11,24 @@ function getGrid() {
         container.appendChild(div);
     }
 };
+
+function hovering() {
+    // let es muy importante.
+    for (let i = 0; i < sqr.length; i++) {
+        sqr[i].addEventListener('mouseover', function () {
+            sqr[i].style.backgroundColor = 'black';
+        })
+    }
+};
+
+function reset() {
+    for (let i = 0; i < sqr.length; i++) {
+        sqr[i].style.backgroundColor = 'rgb(240, 240, 240)';
+    }
+}
+
 getGrid();
+
+hovering();
+
+resetBut.addEventListener('click', reset);
